7d83ecba578031b57dea677d7280f57f8d4477ae
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2026-02-23 17:46:19 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,23 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
     files: ["**/*.js"],
-    ignores: ["node_modules/**"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
     },
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
       eqeqeq: "error",
       "prefer-const": "error",
-      "no-console": "error",
+      "no-console": "off",
     },
   },
 ];
